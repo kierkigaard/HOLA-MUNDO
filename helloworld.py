c27d8b906f622b10d7f4d0933662f33d5b0d9d38
miguel <miguelporta050701@gmail.com>
2024-10-10 09:28:16 -0600

    Añadir helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,14 @@
+#calculadora de dos numeros 
+numero1= int(input("ingresa un numero: "))
+numero2= int(input("ingresa un segundo numero: "))
+resultado= (numero1)+ (numero2)
+print(f"El resultado de la suma es: {resultado}")
+
+#///////////////////
+print("/////////////////")
+
+#INDICE DE MASA COORPORAL
+peso = float(input("ingresa tu peso: "))
+altura = float(input("ingresa tu altura: "))
+imc = peso / altura**2
+print(f"Tu indice de masa coorporal es: {imc}")
